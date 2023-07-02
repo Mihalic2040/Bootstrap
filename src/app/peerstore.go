5f03657a31bb36e2d637171621972d82b30e1585
Mihalic2040 <mihabevza111@gmail.com>
2023-07-02 21:45:22 +0300

    gg

--- src/app/peerstore.go
+++ src/app/peerstore.go
@@ -33,18 +33,33 @@ func ShowPeers(w http.ResponseWriter, r *http.Request) {
 }
 
 func ShowAddrs(w http.ResponseWriter, r *http.Request) {
-	adds := App.Host.Addrs()
-	log.Println(adds)
+	addrs := App.Host.Addrs()
+	log.Println(addrs)
+
+	// Convert multiaddr.Multiaddr to string
+	addrsStr := make([]string, len(addrs))
+	for i, addr := range addrs {
+		addrsStr[i] = addr.String()
+	}
+
+	// Include the PeerID and converted addrs in the data structure
+	data := struct {
+		PeerID string   `json:"peerID"`
+		Addrs  []string `json:"addrs"`
+	}{
+		PeerID: App.Host.ID().Pretty(),
+		Addrs:  addrsStr,
+	}
 
 	w.Header().Set("Content-Type", "application/json")
 
-	// Serialize the adds variable to JSON
-	data, err := json.Marshal(adds)
+	// Serialize the data structure to JSON
+	jsonData, err := json.Marshal(data)
 	if err != nil {
 		http.Error(w, "Failed to serialize JSON", http.StatusInternalServerError)
 		return
 	}
 
 	// Write the JSON data as the response body
-	w.Write(data)
+	w.Write(jsonData)
 }
